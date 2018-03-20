7e2508744cf17be7a7401b7a920ef8a43cf7059d
hong nancy <nancy_hong_123@hotmail.com>
2018-03-19 19:03:16 -0700

    homing senor

---
+++ homing_sensor/homing_sensor.ino
@@ -0,0 +1,40 @@
+#define homePin  38 //active low
+#define dir1 35
+#define dir2 33
+#define pwm 9
+
+
+
+// byte encoderPos;
+bool home;
+
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(homePin, INPUT);
+pinMode(pwm, OUTPUT);
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+    digitalWrite(dir1, LOW);// Stop
+  digitalWrite(dir2, HIGH);
+  analogWrite(pwm, 50);
+}
+
+void loop () {
+
+  home= digitalRead(homePin);
+  if (home){
+    digitalWrite(dir1, LOW);// Stop
+    digitalWrite(dir2, LOW);
+  }
+  else {
+    digitalWrite(dir1, LOW);// Stop
+    digitalWrite(dir2, HIGH); 
+    delay(10);
+    digitalWrite(dir1, LOW);// Stop
+    digitalWrite(dir2, LOW);
+    delay(100);
+
+  }
+}
