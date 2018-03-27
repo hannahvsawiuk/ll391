8b042be5cbf1c536ea65b7c17e3c6af5a8eabb69
Divya Budihal <divyabudihal@gmail.com>
2018-03-26 22:56:50 -0700

    Tested encoder, weird behaviour encountered

--- LaserBot/LaserBot.ino
+++ LaserBot/LaserBot.ino
@@ -4,9 +4,23 @@ Motor x;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  x.encoder.setOe(4);
+  x.encoder.setD(5);
+  x.encoder.setDa0(22);
+  x.encoder.setDa1(23);
+  x.encoder.setDa2(24);
+  x.encoder.setDa3(25);
+  x.encoder.setDa4(26);
+  x.encoder.setDa5(27);
+  x.encoder.setDa6(28);
+  x.encoder.setDa7(29);
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("HI");
+  x.updateAngle();
+  float angle = x.getPos();
+  Serial.println(angle);
 }
